45ee98975eb7d7cafbb9958ce75c305b9c8fbc92
SebP <sepvesar@gmail.com>
2022-04-09 15:46:48 -0400

    Fix bug in playRound
    Fix bug that always returns "You tie!" from playRound

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-function computerPlay(){
+function computerPlay() {
     let hand;
     let randomNum;
-    randomNum = Math.floor(Math.random()*3) + 1; 
+    randomNum = Math.floor(Math.random() * 3) + 1;
     switch (randomNum) {
         case 1:
             hand = "rock";
@@ -9,45 +9,63 @@ function computerPlay(){
         case 2:
             hand = "paper";
             break;
-        
+
         case 3:
             hand = "scissors";
             break;
-    
+
     }
+    
     return hand;
 }
-function playRound(playerSelection, computerSelection){
+function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     let message;
-    switch (playerSelection){
+    switch (playerSelection) {
         case "rock":
-            switch (computerSelection){
+            switch (computerSelection) {
                 case "rock":
                     message = "You Tie!";
+                    break;
                 case "paper":
                     message = "You Lose! Paper beats Rock";
+                    break;
                 case "scissors":
                     message = "You Win! Rock beats Scissors";
+                    break;
             }
+            break;
         case "paper":
-            switch (computerSelection){
+            
+            switch (computerSelection) {
                 case "rock":
                     message = "You Win! Paper beats Rock";
+                    break;
                 case "paper":
                     message = "You Tie!";
+                    break;
                 case "scissors":
-                    message = "You Lose! Scissors beats Paper";
+                    message = "You Lose! Scissors beat Paper";
+                    break;
             }
+            break;
         case "scissors":
-            switch (computerSelection){
+            
+            switch (computerSelection) {
                 case "rock":
                     message = "You Lose! Rock beats Scissors";
+                    break;
                 case "paper":
-                    message = "You Win! Scissors beats Paper";
+                    message = "You Win! Scissors beat Paper";
+                    break;
                 case "scissors":
                     message = "You Tie";
+                    break;
+
             }
+            break;
+
     }
+    
     return message;
 }
