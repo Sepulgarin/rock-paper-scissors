208b4e1cdd0e719beb1d1185c0e0f089ebe762a5
SebP <sepvesar@gmail.com>
2022-04-09 15:25:31 -0400

    Add function that returns if you win or lose game

--- script.js
+++ script.js
@@ -4,16 +4,50 @@ function computerPlay(){
     randomNum = Math.floor(Math.random()*3) + 1; 
     switch (randomNum) {
         case 1:
-            hand = "Rock";
+            hand = "rock";
             break;
         case 2:
-            hand = "Paper";
+            hand = "paper";
             break;
         
         case 3:
-            hand = "Scissors";
+            hand = "scissors";
             break;
     
     }
     return hand;
+}
+function gamePlay(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    let message;
+    switch (playerSelection){
+        case "rock":
+            switch (computerSelection){
+                case "rock":
+                    message = "You Tie!";
+                case "paper":
+                    message = "You Lose! Paper beats Rock";
+                case "scissors":
+                    message = "You Win! Rock beats Scissors";
+            }
+        case "paper":
+            switch (computerSelection){
+                case "rock":
+                    message = "You Win! Paper beats Rock";
+                case "paper":
+                    message = "You Tie!";
+                case "scissors":
+                    message = "You Lose! Scissors beats Paper";
+            }
+        case "scissors":
+            switch (computerSelection){
+                case "rock":
+                    message = "You Lose! Rock beats Scissors";
+                case "paper":
+                    message = "You Win! Scissors beats Paper";
+                case "scissors":
+                    message = "You Tie";
+            }
+    }
+    return message;
 }
