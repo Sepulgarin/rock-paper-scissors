38370d67583ea17d55d33f8fa7bd3af08ba9b8d6
SebP <sepvesar@gmail.com>
2022-04-10 21:48:05 -0400

    Finish game function

--- script.js
+++ script.js
@@ -29,9 +29,11 @@ function playRound(playerSelection, computerSelection) {
                     break;
                 case "paper":
                     message = "You Lose! Paper beats Rock";
+                    computerScore++;
                     break;
                 case "scissors":
                     message = "You Win! Rock beats Scissors";
+                    humanScore++;
                     break;
             }
             break;
@@ -40,12 +42,14 @@ function playRound(playerSelection, computerSelection) {
             switch (computerSelection) {
                 case "rock":
                     message = "You Win! Paper beats Rock";
+                    humanScore++;
                     break;
                 case "paper":
                     message = "You Tie!";
                     break;
                 case "scissors":
                     message = "You Lose! Scissors beat Paper";
+                    computerScore++;
                     break;
             }
             break;
@@ -54,9 +58,11 @@ function playRound(playerSelection, computerSelection) {
             switch (computerSelection) {
                 case "rock":
                     message = "You Lose! Rock beats Scissors";
+                    computerScore++;
                     break;
                 case "paper":
                     message = "You Win! Scissors beat Paper";
+                    humanScore++;
                     break;
                 case "scissors":
                     message = "You Tie";
@@ -71,5 +77,33 @@ function playRound(playerSelection, computerSelection) {
 }
 
 function game(){
-
+    computerScore = 0;
+    humanScore = 0;
+    for (let x = 0; x < 5; x++) {
+        playerSelection = prompt("Please enter Rock, Paper or Scissors","");
+        computerSelection = computerPlay();
+        console.log(`The computer has selected ${computerSelection}`);
+        console.log(playRound(playerSelection, computerSelection));
+        if (computerScore > humanScore) {
+            console.log(`You're losing ${computerScore} to ${humanScore}.`);
+        }
+        else if (humanScore > computerScore){
+            console.log(`You're winning ${humanScore} to ${computerScore}.`);
+        }
+        else{
+            console.log(`You're tied ${humanScore} to ${computerScore}.`);
+        }
+    }
+    if (computerScore > humanScore) {
+        console.log(`You lost ${computerScore} to ${humanScore}.`);
+    }
+    else if (humanScore > computerScore){
+        console.log(`You won ${humanScore} to ${computerScore}.`);
+    }
+    else{
+        console.log(`You tied ${humanScore} to ${computerScore}.`);
+    }
 }
+
+let computerScore;
+let humanScore;
